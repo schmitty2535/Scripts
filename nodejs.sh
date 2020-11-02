f755c778bf8209c5ed92fade8207a83e2531160c
Maxwell Schmitt <50837674+schmitty2535@users.noreply.github.com>
2020-11-02 11:32:37 -0500

    Create nodejs.sh

---
+++ nodejs.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+while true
+do
+echo -n "Please confirm (y or n) :"
+read CONFIRM
+case $CONFIRM in
+y|Y|YES|yes|Yes) break ;;
+n|N|no|NO|No)
+echo Aborting - you entered $CONFIRM
+exit
+;;
+*) echo Please enter only y or n
+esac
+done
+echo You entered $CONFIRM. Continuing ...
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash
